4eadb3bf4e110c8367f515506a99ad88d40f6323
John Wiegley <johnw@newartisans.com>
2015-08-11 13:59:34 -0700

    Simplify and clarify several functions

--- Fusion.hs
+++ Fusion.hs
@@ -11,13 +11,14 @@ module Fusion
     -- * StepList
     , StepList(..)
     -- * Stream
-    , Stream(..), mapS, concatS, fromList, toListS, reduceS, emptyStream
+    , Stream(..), mapS, concatS, fromList, fromListM
+    , toListS, lazyToListS, runEffect, emptyStream
+    , bracketS, next
     -- * StreamList
-    , StreamList(..), concatSL, toListSL, toListSL', eachM
+    , ListT(..), concat
     -- * Pipes
-    , Producer, Pipe, Consumer, Effect
-    , runEffect, each, map, collect
-    , toListM, lazyToListM
+    , Producer, Pipe, Consumer
+    , each, mapP
     )
     where
 
@@ -27,7 +28,7 @@ import Control.Monad.Trans.Class
 import Data.Foldable
 import Data.Functor.Identity
 import Data.Void
-import Prelude hiding (map)
+import Pipes.Safe
 import System.IO.Unsafe
 
 -- Step
@@ -62,7 +63,7 @@ instance Functor m => Functor (Stream a m) where
 instance Monad m => Applicative (Stream a m) where
     pure x = Stream (pure . Done) (pure x)
     {-# INLINE pure #-}
-    sf <*> sx = Stream (pure . Done) (reduceS sf <*> reduceS sx)
+    sf <*> sx = Stream (pure . Done) (runEffect sf <*> runEffect sx)
     {-# INLINE (<*>) #-}
 
 instance MonadTrans (Stream a) where
@@ -98,18 +99,24 @@ concatS (Stream xs i) =
         go' (Yield s' a) = Yield (Right (e, Stream ys (pure s'))) a
 {-# SPECIALIZE concatS :: Stream (Stream a IO r) IO r -> Stream a IO r #-}
 
-fromList :: Applicative m => [a] -> Stream a m ()
+fromList :: Foldable f => Applicative m => f a -> Stream a m ()
 fromList = Stream (\case []     -> pure $ Done ()
-                         (x:xs) -> pure $ Yield xs x) . pure
+                         (x:xs) -> pure $ Yield xs x) . pure . toList
 {-# INLINE fromList #-}
 
-reduceS :: Monad m => Stream a m r -> m r
-reduceS (Stream f i) = i >>= f >>= go
+fromListM :: (Monad m, Foldable f) => m (f a) -> Stream a m ()
+fromListM xs = Stream (\case []     -> return $ Done ()
+                             (y:ys) -> return $ Yield ys y)
+                      (toList <$> xs)
+{-# INLINE fromListM #-}
+
+runEffect :: Monad m => Stream a m r -> m r
+runEffect (Stream f i) = i >>= f >>= go
   where
     go (Done r)    = return r
     go (Skip s)    = f s >>= go
     go (Yield s _) = f s >>= go
-{-# INLINE reduceS #-}
+{-# INLINE runEffect #-}
 
 toListS :: Monad m => Stream a m r -> m [a]
 toListS (Stream f i) = i >>= f >>= go
@@ -119,88 +126,85 @@ toListS (Stream f i) = i >>= f >>= go
     go (Yield s a) = f s >>= liftM (a:) . go
 {-# INLINE toListS #-}
 
+lazyToListS :: Stream a IO r -> IO [a]
+lazyToListS (Stream f i) = i >>= f >>= go
+  where
+    go (Done _)   = return []
+    go (Skip s)    = f s >>= go
+    go (Yield s a) = f s >>= liftM (a:) . unsafeInterleaveIO . go
+{-# INLINE lazyToListS #-}
+
 emptyStream :: Monad m => Stream Void m ()
 emptyStream = pure ()
 {-# INLINE emptyStream #-}
 
--- StreamList
+bracketS :: (Monad m, MonadMask m, MonadSafe m)
+         => Base m s
+         -> (s -> Base m ())
+         -> (forall r. s -> (a -> s -> m r) -> (s -> m r) -> m r -> m r)
+         -> Stream a m ()
+bracketS i f step = Stream go $ mask $ \_unmask -> do
+    s <- liftBase i
+    key <- register (f s)
+    return (s, key)
+  where
+    go (s, key) =
+        step s (\a s' -> return $ Yield (s', key) a)
+               (\s'   -> return $ Skip (s', key))
+               (release key >> (const (Done ()) `liftM` liftBase (f s)))
+{-# SPECIALIZE bracketS
+      :: IO s
+      -> (s -> IO ())
+      -> (forall r. s -> (a -> s -> SafeT IO r)
+           -> (s -> SafeT IO r)
+           -> SafeT IO r
+           -> SafeT IO r)
+      -> Stream a (SafeT IO) () #-}
 
-newtype StreamList m a = StreamList { getStreamList :: Stream a m () }
+next :: Monad m => Stream a m r -> m (Either r (a, Stream a m r))
+next (Stream xs i) = do
+    s <- i
+    x <- xs s
+    case x of
+        Done r     -> return $ Left r
+        Skip s'    -> next (Stream xs (return s'))
+        Yield s' a -> return $ Right (a, Stream xs (return s'))
+{-# SPECIALIZE next :: Stream a IO r -> IO (Either r (a, Stream a IO r)) #-}
 
-instance Functor m => Functor (StreamList m) where
-    fmap f (StreamList s) = StreamList $ mapS f s
+-- ListT
+
+newtype ListT m a = ListT { getListT :: Stream a m () }
+
+instance Functor m => Functor (ListT m) where
+    fmap f (ListT s) = ListT $ mapS f s
     {-# INLINE fmap #-}
 
-instance Monad m => Applicative (StreamList m) where
+instance Monad m => Applicative (ListT m) where
     pure = return
     {-# INLINE pure #-}
     (<*>) = ap
     {-# INLINE (<*>) #-}
 
-instance Monad m => Monad (StreamList m) where
-    return x = StreamList $
-        Stream (\case True  -> return $ Yield False x
-                      False -> return $ Done ())
-               (return True)
+instance Monad m => Monad (ListT m) where
+    return x = ListT $ fromList [x]
     {-# INLINE return #-}
-    m >>= f = concatSL $ fmap f m
+    m >>= f = concatL $ fmap f m
     {-# INLINE (>>=) #-}
 
-concatSL :: Monad m => StreamList m (StreamList m a) -> StreamList m a
-concatSL = StreamList . concatS . getStreamList . fmap getStreamList
-{-# INLINE concatSL #-}
-
-toListSL :: forall m a. Monad m => StreamList m a -> m [a]
-toListSL (StreamList (Stream f i)) = i >>= f >>= go
-  where
-    go (Done ())   = return []
-    go (Skip s)    = f s >>= go
-    go (Yield s a) = f s >>= liftM (a:) . go
-{-# SPECIALIZE toListSL :: StreamList IO a -> IO [a] #-}
-
--- | This does the same as toListSL, but uses 'unsafeInterleaveIO'.
-toListSL' :: StreamList IO a -> IO [a]
-toListSL' (StreamList (Stream f i)) = i >>= f >>= go
-  where
-    go (Done ())   = return []
-    go (Skip s)    = f s >>= go
-    go (Yield s a) = f s >>= liftM (a:) . unsafeInterleaveIO . go
-
-eachM :: (Monad m, Foldable f) => m (f a) -> StreamList m a
-eachM xs = StreamList $ Stream (\case []     -> return $ Done ()
-                                      (y:ys) -> return $ Yield ys y)
-                           (toList <$> xs)
-{-# INLINE eachM #-}
+concatL :: Monad m => ListT m (ListT m a) -> ListT m a
+concatL = ListT . concatS . getListT . fmap getListT
+{-# INLINE concatL #-}
 
 -- Pipes
 
-type Producer   b m r = Stream Void m () -> Stream b    m r
-type Pipe     a b m r = Stream a    m () -> Stream b    m r
-type Consumer a   m r = Stream a    m () -> Stream Void m r
-type Effect       m r = Stream Void m () -> Stream Void m r
-
-runEffect :: Monad m => Effect m r -> m r
-runEffect f = reduceS (f emptyStream)
-{-# INLINE runEffect #-}
+type Producer   b m r = Stream b m r
+type Pipe     a b m r = Stream a m () -> Stream b m r
+type Consumer a   m r = Stream a m () -> m r
 
 each :: (Monad m, Foldable f) => f a -> Producer a m ()
-each xs = const $ Stream (\case []     -> return $ Done ()
-                                (y:ys) -> return $ Yield ys y)
-                         (return (toList xs))
+each = fromList
 {-# INLINE each #-}
 
-map :: Monad m => (a -> b) -> Pipe a b m ()
-map f xs = getStreamList $ fmap f (StreamList xs)
-{-# INLINE map #-}
-
-collect :: Monad m => Consumer a m [a]
-collect xs = lift $ toListSL (StreamList xs)
-{-# INLINE collect #-}
-
-toListM :: Monad m => Producer a m () -> m [a]
-toListM xs = toListSL (StreamList (xs emptyStream))
-{-# INLINE toListM #-}
-
-lazyToListM :: Producer a IO () -> IO [a]
-lazyToListM xs = toListSL' (StreamList (xs emptyStream))
-{-# INLINE lazyToListM #-}
+mapP :: Monad m => (a -> b) -> Pipe a b m ()
+mapP f = getListT . fmap f . ListT
+{-# INLINE mapP #-}
